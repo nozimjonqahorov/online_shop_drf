2e4b08f62644499939e85f3e352d6869a9acf337
Nozimjon <nozimjonkahhorov@gmail.com>
2026-05-23 14:29:57 +0500

    R

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,15 +15,11 @@ from .views import (
 
 urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-
-    # Auth steps
     path("signup/", SignUpView.as_view(), name="signup"),
     path("verify-code/", VerifyCodeView.as_view(), name="verify-code"),
     path("resend-code/", ResendCodeView.as_view(), name="resend-code"),
     path("change-profile-info/", ChangeProfileInfoView.as_view(), name="change-profile-info"),
     path("upload-photo/", UploadProfilePhotoView.as_view(), name="upload-photo"),
-
-    # New APIs
     path("profile/", ProfileView.as_view(), name="profile"),
     path("profile/update/", ProfileUpdateView.as_view(), name="profile-update"),
     path("login/", LoginView.as_view(), name="login"),
